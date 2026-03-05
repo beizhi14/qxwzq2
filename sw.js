35234e7f984ec8d8782a62d051569b379fa5a0d6
beizhi14 <2769179121@qq.com>
2026-03-05 23:35:02 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+﻿const CACHE_NAME = 'gobang-pwa-v1';
+const RESOURCES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/nina.png',
+  '/bjyy.mp3'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(RESOURCES_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
